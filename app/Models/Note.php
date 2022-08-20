da9886a043c0f0d5c6baf037bdf58d531d1e2a17
Aziz Rabhi <rabhiaziz@gmail.com>
2022-08-20 14:12:50 +0200

    set up migration and models and controllers

---
+++ app/Models/Note.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+    protected $table= 'notes';
+    protected $fillable = [
+        'note',
+        'is_public',
+        'user_id'
+    ];
+}
---
+++ database/migrations/2022_08_20_115906_create_comments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('comment');
+
+            $table->unsignedBigInteger('note_id');
+            $table->foreign('note_id')->references('id')->on('notes');
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};
